af384493888d6ccf8b1bd81b46c0764d02533ea1
shivprajapat <shivkumarprajapat232@gmail.com>
2023-07-22 18:10:19 +0530

    refactor: 💡 header & footer ui

---
+++ components/CustomButton.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { CustomButtonProps } from "@/types";
+import Image from "next/image";
+
+const defaultStyles =
+    "flex flex-row relative justify-center items-center py-3 px-6 outline-none";
+const CustomButton = ({
+    isDisabled,
+    btnType,
+    containerStyles,
+    textStyles,
+    title,
+    rightIcon,
+    handleClick,
+}: CustomButtonProps) => {
+    return (
+        <button
+            disabled={isDisabled}
+            type={btnType || "button"}
+            className={`${defaultStyles} ${containerStyles}`}
+            onClick={handleClick}
+        >
+            <span className={`flex-1 ${textStyles}`}>{title}</span>
+            {rightIcon && (
+                <div className="relative w-6 h-6">
+                    <Image
+                        src={rightIcon}
+                        alt="arrow_left"
+                        fill
+                        className="object-contain"
+                    />
+                </div>
+            )}
+        </button>
+    );
+};
+
+export default CustomButton;
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,27 +2,27 @@ import Image from "next/image";
 import React from "react";
 
 const Hero = () => {
-    return (
-        <div className="hero">
-            <div className="flex-1 pt-36 padding-x">
-                <h1 className="hero__title">
-                    Find, book, rent a car—quick and super easy!
-                </h1>
+  return (
+    <div className="flex xl:flex-row flex-col gap-5 relative z-0 max-w-[1440px] mx-auto">
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="2xl:text-[72px] sm:text-[64px] text-[50px] font-extrabold">
+          Find, book, rent a car—quick and super easy!
+        </h1>
 
-                <p className="hero__subtitle">
-                    Streamline your car rental experience with our effortless booking
-                    process.
-                </p>
-            </div>
-            <div className="hero__image-container">
-                <div className="hero__image">
-                    <Image src="/hero.png" alt="hero" fill className="object-contain" />
-                </div>
-
-                <div className="hero__image-overlay" />
-            </div>
+        <p className="text-[27px] text-black-100 font-light mt-5">
+          Streamline your car rental experience with our effortless booking
+          process.
+        </p>
+      </div>
+      <div className="xl:flex-[1.5] flex justify-end items-end w-full xl:h-screen">
+        <div className="relative xl:w-full w-[90%] xl:h-full h-[590px] z-0">
+          <Image src="/hero.png" alt="hero" fill className="object-contain" />
         </div>
-    );
+
+        <div className="absolute xl:-top-24 xl:-right-1/2 -right-1/4 bg-hero-bg bg-repeat-round -z-10 w-full xl:h-screen h-[590px] overflow-hidden" />
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
