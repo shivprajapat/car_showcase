50a35dafd7a7ad462cbfbce8b5dce84682324f8f
shivprajapat <shivkumarprajapat232@gmail.com>
2023-07-25 12:51:43 +0530

    refactor: 💡 generate car image

--- utils/index.ts
+++ utils/index.ts
@@ -21,7 +21,7 @@ export async function fetchCars() {
     "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
   };
   const response = await fetch(
-    "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla",
+    "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=q3",
     {
       headers: headers,
     }
@@ -36,5 +36,15 @@ export async function fetchCars() {
 export const generateCarImageUrl = (car: CarProps, angle?: string) => {
   const url = new URL("https://cdn.imagin.studio/getimage");
   const { make, model, year } = car;
+  url.searchParams.append(
+    "customer", "hrjavascript-mastery"
+  );
+  url.searchParams.append("make", make);
+  url.searchParams.append("modelFamily", model.split(" ")[0]);
+  url.searchParams.append("zoomType", "fullscreen");
+  url.searchParams.append("modelYear", `${year}`);
+  // url.searchParams.append('zoomLevel', zoomLevel);
+  url.searchParams.append("angle", `${angle}`);
+
   return `${url}`;
 };
